cd1c6af39f30871a868bacc39cb51740accf6b21
ravan234 <rv12884@gmail.com>
2023-01-31 15:07:13 +0530

    31 Jan 2023
    dekh le code ab thik h

--- lib/main.dart
+++ lib/main.dart
@@ -3,40 +3,38 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-  title: "CBFC",
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('My App Bar'),
-        centerTitle:true ,
-        backgroundColor: Colors.black26,
-        titleTextStyle: TextStyle(fontSize: 20),
-      ),
-      body: Image.asset('images/bb.png',
-      height: 300,),
-      // (
-      //     child:Text('My Home Page',
-      //     style: TextStyle(
-      //       fontStyle: FontStyle.normal,
-      //       fontSize: 25
-      //     ),)
-      // ),
-      floatingActionButton: FloatingActionButton(onPressed: ()=>
-          print("You clicked me"),
-        child: Text("click"),
-        backgroundColor: Colors.green,
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.red,
-        child: Container(
-          height: 40,
-          child: Center(
-            child:Regular(text: 'text', size: 20, color: Colors.red,
-            )
-        )
-      ),
-    ),
-  )
-  )
-  );
+      title: "CBFC",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My App Bar'),
+          centerTitle: true,
+          backgroundColor: Colors.black26,
+          titleTextStyle: const TextStyle(fontSize: 20),
+        ),
+        body: Image.asset('assets/images/bb.png', height: 300, width: 300),
+        // (
+        //     child:Text('My Home Page',
+        //     style: TextStyle(
+        //       fontStyle: FontStyle.normal,
+        //       fontSize: 25
+        //     ),)
+        // ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => print("You clicked me"),
+          backgroundColor: Colors.green,
+          child: const Text("click"),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.red,
+          child: SizedBox(
+              height: 40,
+              child: Center(
+                  child: Regular(
+                text: 'text',
+                size: 20,
+                color: Colors.red,
+              ))),
+        ),
+      )));
 }
